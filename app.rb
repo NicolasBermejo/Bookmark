eaa48df393e0d38bee5e8e4fa1b19b29dde44602
Nicolas Bermejo <nicbermejo@gmail.com>
2021-02-22 16:55:06 +0100

    set up the SQL database

--- app.rb
+++ app.rb
@@ -7,6 +7,9 @@ class Bookmark < Sinatra::Base
     erb :index
   end
 
+  get '/bookmarks' do
+    erb :bookmark_list
+  end
 
   run! if app_file == $0
 end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -4,3 +4,12 @@ feature 'Viewing bookmarks' do
     expect(page).to have_content "Bookmark Manager"
   end
 end
+
+feature 'viewing bookmark links' do
+  scenario 'viewing the bookmark links' do
+    visit('/')
+    click_button "Show Bookmarks"
+    expect(page).to have_content "Google Chrome"
+    expect(page).to have_content "https://www.google.com/"
+  end
+end
